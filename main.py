c6a56db89318b0aa51a174a942140f72e6b4111f
virgilvox <mzara1@asu.edu>
2019-01-06 22:51:29 -0700

    sets environment var better

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ print(ser.name)         # check which port was really used
 ser.write('MFU\r\n')     # write a string
 s = ser.read(512)
 print(s)
+ser.write('RF 0\r\n')
 
 os.putenv('NEWOTP',s)
 os.system('bash')

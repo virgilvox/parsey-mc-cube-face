1e2d26f74bc95d4c13500ffcf3440f81b15834e6
virgilvox <mzara1@asu.edu>
2019-01-06 22:18:25 -0700

    sets environment var

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
 import serial
+
 ser = serial.Serial('/dev/tty.usbserial-DN05Z2JS', 115200)  # open serial port
 print(ser.name)         # check which port was really used
 ser.write('MFU\r\n')     # write a string
 s = ser.read(512)
 print(s)
+
+os.putenv('NEWOTP',s)
+os.system('bash')
+
 ser.close()

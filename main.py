00f1200e9e1542b974a7f1cf6cdfdccbe08e803b
virgilvox <mzara1@asu.edu>
2019-01-06 18:10:24 -0700

    initial

---
+++ main.py
@@ -0,0 +1,7 @@
+import serial
+ser = serial.Serial('/dev/tty.usbserial-DN05Z2JS', 115200)  # open serial port
+print(ser.name)         # check which port was really used
+ser.write('MFU\r\n')     # write a string
+s = ser.read(512)
+print(s)
+ser.close()
